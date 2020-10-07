bcf10adfcdef97497aefb8c4e29e813d37b8ebfc
KDS-Limitless <limitlesskds@gmail.com>
2020-10-07 21:35:19 +0100

    declared constant strings in user resource

--- resources/user.py
+++ resources/user.py
@@ -1,27 +1,34 @@
 import traceback
-from flask_jwt_extended import (
-    create_access_token, 
-    create_refresh_token,
-    get_jwt_identity, 
-    jwt_required, 
-    get_raw_jwt,
-    jwt_refresh_token_required, 
-    fresh_jwt_required, 
-    verify_fresh_jwt_in_request, 
-    verify_jwt_refresh_token_in_request
-)
+
 from flask import make_response, render_template
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                fresh_jwt_required, get_jwt_identity,
+                                get_raw_jwt, jwt_refresh_token_required,
+                                jwt_required, verify_fresh_jwt_in_request,
+                                verify_jwt_refresh_token_in_request)
 from flask_restful import Resource, request
 from marshmallow import ValidationError
 from werkzeug.security import safe_str_cmp
 
 from db import db
-from models.user import UserModel, TokenBlacklist
-from schemas.user import UserSchema
+from models.user import TokenBlacklist, UserModel
 from password import psw
+from schemas.user import UserSchema
 from utils.mailgun import MailgunException
 
+
+USER_NOT_FOUND = "User not Found!"
+USER_DELETED = "User {} has been deleted"
+EMAIL_TAKEN = "{} has been taken by another user"
+EMAIL_DOES_NOT_EXIST = "{} does not exist in our database"
+USER_CREATED = "Account created for {}. check your email for procedures to activate your account."
+INTERNAL_SERVER_ERROR = "We are having some issues with our servers at the moment. Please come back later" 
+NOT_ACTIVATED = "Account for { has not yet been activated. Click the link sent to your email to activate your account."
+INCORRECT_PASSWORD = "Is this your password?"
+LOGGED_OUT = "You have been logged out."
+
 user_schema = UserSchema(many=True)
+
 
 class User(Resource):
  
@@ -30,7 +37,7 @@ class User(Resource):
     def get(cls, name):
         get_user = UserModel.find_user_by_name(name) 
         if not get_user:
-            return {'Message': "User Not Found"}, 404
+            return {'message': USER_NOT_FOUND }, 404
         print(user_schema.dump(get_user))
         return user_schema.dump(get_user), 200 
         
@@ -47,16 +54,16 @@ class User(Resource):
 
             db.session.commit()
             return user_schema.dump(find_user), 200
-        return {"message": "User not found"}, 400
+        return {"message": USER_NOT_FOUND}, 400
            
     @classmethod
     @fresh_jwt_required
     def delete(cls, name):
         user = UserModel.find_first_user_by_name(name)
         if not user:
-            return {"Message": "User Not Found"}, 400
+            return {"Message": USER_NOT_FOUND }, 400
         user.delete_from_db()
-        return {"message": "User Deleted!"},200
+        return {"message": USER_DELETED.format(user.username)},200
 
 
 class UserRegister(Resource):
@@ -65,14 +72,14 @@ class UserRegister(Resource):
         new_user = request.get_json()
 
         if UserModel.find_user_by_email(new_user['email']):
-            return {"message": "A User already exists with that email"}, 404
+            return {"message": EMAIL_TAKEN.format(new_user['email'])}, 404
         password = psw.generate_password_hash(new_user['password'])
         user = UserModel(new_user['username'], password ,new_user['email'])
 
         try:
             user.save_to_db()
             user.send_email()
-            return {"Message": f"User {new_user['username']} Created Sucessfully. Check your email for your confirmation token"}, 200
+            return {"Message": USER_CREATED.format(new_user['username'])}, 200
 
         except MailgunException as e:
             user.delete_from_db()
@@ -81,7 +88,7 @@ class UserRegister(Resource):
         except:
             traceback.print_exc()
             user.delete_from_db()
-            return {"message": "Internal Server Error"}, 500
+            return {"message": INTERNAL_SERVER_ERROR}, 500
         
 
 
@@ -92,7 +99,7 @@ class UserLogin(Resource):
         get_user_from_db = UserModel.find_user_by_email(get_user_details['email'])
 
         if not get_user_from_db:
-            return {"message": f"Invalid Email Address. A user with {get_user_details['email']} does not exist in the database"}, 404
+            return {"message": EMAIL_DOES_NOT_EXIST.format(get_user_details['email'])}, 404
         
         if psw.check_password_hash(get_user_from_db.password, get_user_details['password']):
             if get_user_from_db.is_activated:
@@ -104,8 +111,8 @@ class UserLogin(Resource):
                     "Refresh_Token": refresh_token
                 }, 200
 
-            return {"message": "Your account has not been activated "}, 400
-        return {"message": "Incorrect Password"}, 401
+            return {"message": NOT_ACTIVATED.format(get_user_from_db.email)}, 400
+        return {"message": INCORRECT_PASSWORD}, 401
 
 
 class UserLogout(Resource): 
@@ -116,9 +123,9 @@ class UserLogout(Resource):
         try:
             revoked_token = TokenBlacklist(jti = jti)
             revoked_token.add()
-            return {"message": "You have been logged out"}, 200
+            return {"message": LOGGED_OUT}, 200
         except:
-            return {"message": "Something went wrong"}, 500
+            return {"message": INTERNAL_SERVER_ERROR}, 500
         
     
 class TokenRefresh(Resource):
@@ -138,7 +145,7 @@ class UserConfirmation(Resource):
         print(find_user.email)
 
         if not find_user:
-            return {"message": "User not found"}, 404
+            return {"message": USER_NOT_FOUND}, 404
         
         find_user.is_activated = True
         find_user.save_to_db()
