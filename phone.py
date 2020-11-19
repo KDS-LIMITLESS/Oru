1b7894eb20e92df277c74f858766a65e6beb480b
KDS-Limitless <limitlesskds@gmail.com>
2020-11-19 10:46:24 +0100

    refactored Country class

--- phone.py
+++ phone.py
@@ -1,43 +1,57 @@
 import iso3166
 import phonenumbers
 from phonenumbers import is_valid_number_for_region, phonenumberutil
-from typing import List
-
-
+import requests
+import json
 
 
 class Country:
 
 
-    @classmethod
-    def get_country_details(cls, country_name):
+    def get_country_name(self, country_name):
         c_name = country_name.upper()
         try:
             country_name = iso3166.countries[c_name]
-            return country_name.name.upper()
+            self.country_name = country_name
+            return self.country_name.name.upper()
         except KeyError:
             return f"{country_name} not found!"
 
-    @classmethod
-    def get_country_region(cls, country):
-        c = country.upper()
+    def get_country_region(self):
         try:
-            country_name = iso3166.countries[c]
-            region = country_name.alpha2
-            return region
+            #country_name = iso3166.countries[c]
+            self.region = self.country_name.alpha2
+            return self.region
         except KeyError:
-            return f"Region for {country} Not found"
+            return f"Region for {self.country_name} Not found"
         except phonenumbers.phonenumberutil.NumberParseException:
             return "Region not found!"
         
-
-    @classmethod
-    def get_user_phonenumber(cls, phonenumber:str, country):
+    def get_user_phonenumber(self, phonenumber:str):
         try:
-            country_region = cls.get_country_region(country)
-            phone = phonenumbers.parse(phonenumber,country_region)
+            #country_region = cls.get_country_region(country)
+            phone = phonenumbers.parse(phonenumber,self.region)
         except phonenumbers.phonenumberutil.NumberParseException:
             return f"Please check the country name for mispelt words"    
-        if is_valid_number_for_region(phone,country_region):
+        if is_valid_number_for_region(phone,self.region):
             return phonenumbers.format_number(phone,phonenumbers.PhoneNumberFormat.INTERNATIONAL)
         return f"Invalid Number"
+
+    def get_states(self):
+        country_id = self.region  #get_country_region(country)
+        resp = requests.get(f"http://naijacrawl.com/api/api/v2/free-get-state-list?country_code={country_id}").text.capitalize()
+        # convert the Response object to dict with json.loads()
+        self.resp_dict = json.loads(resp)
+        return self.resp_dict
+
+    def get_city(self, state):
+        for state_id, state_name in self.resp_dict.items():
+            if state.lower() == state_name:
+                response = requests.get(f"http://naijacrawl.com/api/api/v2/free-get-city-list?state_id={state_id}").text.capitalize()
+                response_dict = json.loads(response)
+                return response_dict
+        
+        return f"{state} is not a state in {self.country_name.name} "
+            
+
+
