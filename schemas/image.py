04e7143a442c5693c81a0ae8b8a077a876736c6c
KDS-Limitless <limitlesskds@gmail.com>
2020-11-25 21:06:45 +0100

    Feat: Support for Image Uploads

---
+++ schemas/image.py
@@ -0,0 +1,18 @@
+from marshmallow import Schema, fields
+from werkzeug.datastructures import FileStorage
+
+
+class FileStorageField(fields.Field):
+
+
+    def _deserialize(self, value, attr, data, **kwargs) -> FileStorage:
+        if value is None:
+            return None
+        if not isinstance(value, FileStorage):
+            self.fail("invalid")
+
+        return value
+        
+
+class ImageSchema(Schema):
+    image = FileStorageField(required=True, error_messages={"invalid": "Not a valid Image"})
