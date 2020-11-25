9c4ec42c9e0bee7368a1b50264b40d8cc86b2f3e
KDS-Limitless <limitlesskds@gmail.com>
2020-11-25 21:04:56 +0100

    Feat: Configured to accept image uploads

--- default_config.py
+++ default_config.py
@@ -5,6 +5,7 @@ DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///site'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+UPLOADED_IMAGES_DEST = os.path.join("static", "images")
 JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
 JWT_ERROR_MESSAGE_KEY = "Error"
 JWT_BLACKLIST_ENABLED = True
