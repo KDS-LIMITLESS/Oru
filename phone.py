37af4ba6cb9dfafbc35a2e04a92b63512cbd1c72
KDS-Limitless <limitlesskds@gmail.com>
2021-01-02 21:54:01 +0100

    Update: Updated phone class / Bug Fix

--- phone.py
+++ phone.py
@@ -16,8 +16,12 @@ class Country:
             return self.country_name.name.upper()
         except KeyError:
             return f"{country_name} not found!"
-
+        
     def get_country_region(self):
+        try:
+            self.country_name
+        except AttributeError:
+            return "."
         try:
             #country_name = iso3166.countries[c]
             self.region = self.country_name.alpha2
@@ -29,6 +33,11 @@ class Country:
         
     def get_user_phonenumber(self, phonenumber:str):
         try:
+            self.region
+        except AttributeError:
+            return "."
+            
+        try:
             #country_region = cls.get_country_region(country)
             phone = phonenumbers.parse(phonenumber,self.region)
         except phonenumbers.phonenumberutil.NumberParseException:
@@ -37,21 +46,31 @@ class Country:
             return phonenumbers.format_number(phone,phonenumbers.PhoneNumberFormat.INTERNATIONAL)
         return f"Invalid Number"
 
-    def get_states(self):
+    def get_states(self, state_name:str):
+        try:
+            self.region
+        except AttributeError:
+            return "."
         country_id = self.region  #get_country_region(country)
-        resp = requests.get(f"http://naijacrawl.com/api/api/v2/free-get-state-list?country_code={country_id}").text.capitalize()
+        resp = requests.get(f"http://naijacrawl.com/api/api/v2/free-get-state-list?country_code={country_id}").text
         # convert the Response object to dict with json.loads()
         self.resp_dict = json.loads(resp)
-        return self.resp_dict
-
-    def get_city(self, state):
-        for state_id, state_name in self.resp_dict.items():
-            if state.lower() == state_name:
-                response = requests.get(f"http://naijacrawl.com/api/api/v2/free-get-city-list?state_id={state_id}").text.capitalize()
-                response_dict = json.loads(response)
-                return response_dict
+        for self.state_id, self.state in self.resp_dict.items():
+            if state_name.capitalize() == self.state:
+                return self.state    
+        return f"{state_name.capitalize()} is not a state in {self.country_name[0]}"
         
-        return f"{state} is not a state in {self.country_name.name} "
+
+    def get_city(self, city_name):
+        try:
+            self.state_id
+        except AttributeError:
+            return "."
+        state_id = self.state_id
+        response = requests.get(f"http://naijacrawl.com/api/api/v2/free-get-city-list?state_id={state_id}").text.capitalize()
+        response_dict = json.loads(response)
+        for _, city in response_dict.items():
+            if city_name.lower() == city:
+                return city.capitalize()    
+        return f"{city_name.capitalize()} is not a city in {self.state} "
             
-
-
