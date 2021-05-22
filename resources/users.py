2a572bd176e0afbd457b3ed74217fb817486cece
KDS-Limitless <limitlesskds@gmail.com>
2021-05-22 07:53:04 +0100

    Updated routes & Fixed bugs in update routes

--- resources/users.py
+++ resources/users.py
@@ -14,7 +14,7 @@ from quart_jwt_extended import (create_access_token, create_refresh_token,
 from quart_openapi import PintBlueprint, Resource
 from requests_async.adapters import ConnectionError
 from schema.users import (EmailSchema, LocationSchema, PasswordSchema,
-                         UserLoginSchema, UsernameSchema, UserSchema)
+                          UserLoginSchema, UsernameSchema, UserSchema)
 from strings.constants import (ACCOUNT_UPDATED, EMAIL_TAKEN, EMAIL_UPDATED,
                                INCORRECT_EMAIL_OR_PASSWORD, LOGGED_OUT,
                                NOT_ACTIVATED, USER_CREATED, USER_DELETED,
@@ -36,7 +36,7 @@ class User(Resource):
         return user_schema.dump(get_user), 200
 
 
-@user.route('/delete')
+@user.route('/users/delete')
 class DeleteUser(Resource):
     @classmethod
     @fresh_jwt_required
@@ -49,18 +49,18 @@ class DeleteUser(Resource):
         return {"message": USER_DELETED.format(user.username)}, 200
 
 
-@user.route('/updates/username')
+@user.route('/users/updates/username')
 class UpdateUserUsername(Resource):
     @fresh_jwt_required
     async def put(self):
         username_schema = UsernameSchema()
         user_identity = get_jwt_identity()
-        current_user = UserModel.find_user_by_email(user_identity)
+        current_user = await UserModel.find_user_by_email(user_identity)
 
         if current_user:
-            user = await request.get_json()
+            newUsername = await request.get_json()
             try:
-                get_user = username_schema.load(user)
+                get_user = username_schema.load(newUsername)
             except ValidationError as err:
                 return err.messages, 404
 
@@ -72,25 +72,24 @@ class UpdateUserUsername(Resource):
         return {"message": USER_NOT_FOUND}, 404
 
 
-@user.route('/updates/email')
+@user.route('/users/updates/email')
 class UpdateUserEmail(Resource):
     @fresh_jwt_required
     async def put(self):
         email_schema = EmailSchema()
-        user_identity = get_jwt_identity() 
-        current_user = UserModel.find_user_by_email(user_identity)
+        user_identity = get_jwt_identity()
+        current_user = await UserModel.find_user_by_email(user_identity)
 
         if current_user:
-            user = await request.get_json()
+            newEmail = await request.get_json()
             try:
-                get_user = email_schema.load(user)
+                get_user = email_schema.load(newEmail)
             except ValidationError as err:
                 return err.messages, 404
 
             try:
                 current_user.email = get_user["email"]
-
-                if get_user['email'] == current_user.email or UserModel.find_user_by_email(get_user['email']):
+                if get_user['email'] == current_user.email or await UserModel.find_user_by_email(get_user['email']):
                     return {"message": EMAIL_TAKEN.format(get_user['email'])}, 400
 
                 new_confirmation = UserConfirmationModel(current_user.id)
@@ -100,19 +99,20 @@ class UpdateUserEmail(Resource):
                 db.session.commit()
                 return {"message": EMAIL_UPDATED}, 200
 
-            except (MailgunException, traceback.print_exc()) as err:
+            except (MailgunException, Exception) as err:
                 return {'message': str(err)}, 500
 
         return {"message": USER_NOT_FOUND}, 404
 
 
-@user.route('/updates/password')
+# drop this function or refactor to reset password.
+@user.route('/users/updates/password')
 class UpdateUserPassword(Resource):
     @fresh_jwt_required
     async def put(self):
         password_schema = PasswordSchema()
-        user_identity = get_jwt_identity() 
-        current_user = UserModel.find_user_by_email(user_identity) 
+        user_identity = get_jwt_identity()
+        current_user = await UserModel.find_user_by_email(user_identity)
 
         if current_user:
             user = await request.get_json()
@@ -129,24 +129,24 @@ class UpdateUserPassword(Resource):
         return {"message": USER_NOT_FOUND}, 404
 
 
-@user.route('/updates/location')
+@user.route('/users/updates/location')
 class UpdateUserLocation(Resource):
     @fresh_jwt_required
     async def put(self):
         location_schema = LocationSchema()
         user_identity = get_jwt_identity() 
-        current_user = UserModel.find_user_by_email(user_identity)
+        current_user = await UserModel.find_user_by_email(user_identity)
 
         if current_user:
-            user = await request.get_json()
+            currentLocation = await request.get_json()
             try:
-                get_user = location_schema.load(user)
+                get_user = location_schema.load(currentLocation)
             except ValidationError as err:
                 return err.messages, 404
 
-            current_user.country = Country.get_country_name(Country, get_user['country'])
-            _ = Country.get_country_region(Country)
-            current_user.phone_number = Country.get_user_phonenumber(Country, get_user["phone_number"])
+            current_user.country = await Country.get_country_name(Country, get_user['country'])
+            _ = await Country.get_country_region(Country)
+            current_user.phone_number = await Country.get_user_phonenumber(Country, get_user["phone_number"])
             current_user.state = await Country.get_states(Country, get_user['state'])
             current_user.city = await Country.get_city(Country, get_user['city'])
 
@@ -224,11 +224,11 @@ class UserLogout(Resource):
             revoked_token = TokenBlacklist(jti=jti)
             revoked_token.add()
             return {"message": LOGGED_OUT}, 200
-        except (EnvironmentError, ConnectionError) as e:
+        except (ConnectionError) as e:
             return {"message": str(e)}, 500
 
 
-@user.route('/token/refresh')
+@user.route('/access_token_refresh')
 class TokenRefresh(Resource):
     @classmethod
     @jwt_refresh_token_required
--- user_confirmation/schema.py
+++
@@ -1,9 +0,0 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from models.user_confirmation import UserConfirmationModel
-
-
-class UserConfirmationSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = UserConfirmationModel
-        load_only = ("user",)
-        include_fk = True
